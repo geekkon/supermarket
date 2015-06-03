cc5e4e2a2acb887fb14d4ad65f503b7512064885
Dim <dim@MacBook-Pro-Dim.local>
2015-06-03 14:55:12 +0400

    Initial Commit

---
+++ NewSupermarket/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewSupermarket
+//
+//  Created by Dim on 03.06.15.
+//  Copyright (c) 2015 Dmitriy Baklanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewSupermarket/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewSupermarket
+//
+//  Created by Dim on 03.06.15.
+//  Copyright (c) 2015 Dmitriy Baklanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
